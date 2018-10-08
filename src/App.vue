f269f367dd024a2370b6e3ab25f1fd9874d032a5
Duy Hao <hao.nguyen@vitopmedia.co>
2018-10-08 15:04:46 +0700

    first source

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app id="inspire">
+        <Menu/>
+        <!--<img src="./assets/logo.png">-->
+        <v-content>
+            <v-container>
+                <router-view/>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    import MenuLayout from "./components/Menu"
+
+    export default {
+        name: 'App',
+        components: {
+            'Menu': MenuLayout
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
